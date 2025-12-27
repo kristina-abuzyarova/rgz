b71e62ef2bb829bab763be4af83808a1f71abe11
Абузярова Кристина <kristina19283746@gmail.com>
2025-12-27 11:16:27 +0700

    'new'

--- initiatives-project/app.py
+++ initiatives-project/app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 import os
-import randomg
+import random
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key-change-in-production'
